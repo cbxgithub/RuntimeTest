a4cd36eb571f19c55141b33702429f779d459645
贺廷濬 <hetingjun@hetingjundeMacBook-Air.local>
2017-11-02 16:35:15 +0800

    Initial Commit

---
+++ RuntimeTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RuntimeTest
+//
+//  Created by 贺廷濬 on 2017/11/2.
+//  Copyright © 2017年 cbx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
